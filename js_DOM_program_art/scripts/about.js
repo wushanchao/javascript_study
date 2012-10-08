51a5b6ee6581ef5c4c8c0e2a9a451bebaa53f8cb
sam <wushanchao@gmail.com>
2012-10-08 22:40:23 +0800

    javascript dom 编程艺术

---
+++ js_DOM_program_art/scripts/about.js
@@ -0,0 +1,32 @@
+// 根据元素id展示不同内容
+function showSection(id) {
+  var divs = document.getElementsByTagName("div");
+  for (var i=0; i<divs.length; i++ ) {
+    if (divs[i].className.indexOf("section") == -1) continue;
+    if (divs[i].getAttribute("id") != id) {
+      divs[i].style.display = "none";
+    } else {
+      divs[i].style.display = "block";
+    }
+  }
+}
+
+function prepareInternalnav() {
+  if (!document.getElementsByTagName) return false;
+  if (!document.getElementById) return false;
+  if (!document.getElementById("internalnav")) return false;
+  var nav = document.getElementById("internalnav");
+  var links = nav.getElementsByTagName("a");
+  for (var i=0; i<links.length; i++ ) {
+    var sectionId = links[i].getAttribute("href").split("#")[1];//取出链接#后面的字符串
+    if (!document.getElementById(sectionId)) continue;
+    document.getElementById(sectionId).style.display = "none";
+    links[i].destination = sectionId;
+    links[i].onclick = function() {
+      showSection(this.destination);
+      return false;
+    }
+  }
+}
+
+addLoadEvent(prepareInternalnav);
---
+++ js_DOM_program_art/scripts/live.js
@@ -0,0 +1,69 @@
+// 隔行变色，不是通过%2运算，而是true和false的标志来实现
+function stripeTables() {
+  if (!document.getElementsByTagName) return false;
+  var tables = document.getElementsByTagName("table");
+  for (var i=0; i<tables.length; i++) {
+    var odd = false;
+    var rows = tables[i].getElementsByTagName("tr");
+    for (var j=0; j<rows.length; j++) {
+      if (odd == true) {
+        addClass(rows[j],"odd");
+        odd = false;
+      } else {
+        odd = true;
+      }
+    }
+  }
+}
+
+// 鼠标移入，该行高亮，移出高亮消失
+function highlightRows() {
+  if(!document.getElementsByTagName) return false;
+  var rows = document.getElementsByTagName("tr");
+  for (var i=0; i<rows.length; i++) {
+    rows[i].oldClassName = rows[i].className
+    rows[i].onmouseover = function() {
+      addClass(this,"highlight");
+    }
+    rows[i].onmouseout = function() {
+      this.className = this.oldClassName
+    }
+  }
+}
+// 根据表格的abbr标签内容，生成dl,dt元素
+function displayAbbreviations() {
+  if (!document.getElementsByTagName || !document.createElement || !document.createTextNode) return false;
+  var abbreviations = document.getElementsByTagName("abbr");
+  if (abbreviations.length < 1) return false;
+  var defs = new Array();
+  for (var i=0; i<abbreviations.length; i++) {
+    var current_abbr = abbreviations[i];
+    if (current_abbr.childNodes.length < 1) continue;
+    var definition = current_abbr.getAttribute("title");
+    var key = current_abbr.lastChild.nodeValue;
+    defs[key] = definition;
+  }
+  var dlist = document.createElement("dl");
+  for (key in defs) {
+    var definition = defs[key];
+    var dtitle = document.createElement("dt");
+    var dtitle_text = document.createTextNode(key);
+    dtitle.appendChild(dtitle_text);
+    var ddesc = document.createElement("dd");
+    var ddesc_text = document.createTextNode(definition);
+    ddesc.appendChild(ddesc_text);
+    dlist.appendChild(dtitle);
+    dlist.appendChild(ddesc);
+  }
+  if (dlist.childNodes.length < 1) return false;
+  var header = document.createElement("h3");
+  var header_text = document.createTextNode("Abbreviations");
+  header.appendChild(header_text);
+  var container = document.getElementById("content");
+  container.appendChild(header);
+  container.appendChild(dlist);
+}
+
+addLoadEvent(stripeTables);
+addLoadEvent(highlightRows);
+addLoadEvent(displayAbbreviations);
